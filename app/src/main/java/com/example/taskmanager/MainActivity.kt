5206b73386c06114bc014ccb1275127570ee6e08
AndreLeite121 <andre.leite@ufvjm.edu.br>
2023-09-02 00:09:58 -0300

    TeskManager

---
+++ app/src/main/java/com/example/taskmanager/MainActivity.kt
@@ -0,0 +1,126 @@
+package com.example.taskmanager
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.taskmanager.ui.theme.TaskManagerTheme
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TaskManagerTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    GreetingImage(Titulo = "All tasks completed", SubTituilo = "Nice work!")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun GreetingText(Titulo: String, SubTituilo: String, modifier: Modifier = Modifier) {
+    Column (
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+
+        modifier= Modifier
+            //.padding(16.dp)
+
+
+    ){
+
+        Text(
+            text = Titulo,
+            style = TextStyle
+                (
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center
+            ),
+            modifier = Modifier
+                .align(alignment = Alignment.Start)
+                .padding(
+                    top = 24.dp,
+                    bottom = 8.dp
+                )
+            )
+
+
+        Text(
+            text = SubTituilo,
+            textAlign = TextAlign.Center,
+            fontSize = 16.sp,
+            modifier = Modifier
+                .align(alignment = Alignment.CenterHorizontally)
+
+
+        )
+
+    }
+
+}
+
+@Composable
+fun GreetingImage(Titulo: String, SubTituilo: String, modifier: Modifier = Modifier){
+    val image = painterResource(R.drawable.ic_task_completed)
+
+    Box(
+            modifier = Modifier
+                .fillMaxSize(),
+    contentAlignment = Alignment.Center
+            )
+    {
+        Column (
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
+            Image(
+                painter = image,
+                contentDescription = null
+            )
+            GreetingText(
+                Titulo = Titulo,
+                SubTituilo = SubTituilo,
+                modifier = Modifier
+                    .fillMaxSize()
+
+
+            )
+        }
+
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    TaskManagerTheme {
+        GreetingImage(Titulo = "All tasks completed", SubTituilo = "Nice work!")
+    }
+}
